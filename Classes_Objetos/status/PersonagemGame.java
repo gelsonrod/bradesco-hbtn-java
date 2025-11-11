ab52f28502e1f87ab95bcd5d5ae1fb83b58a0160
gelsonrod <ggr.cursos@gmail.com>
2025-11-11 10:57:19 -0300

    Add Task 3: Status do Personagem - Add status attribute with getter

---
+++ Classes_Objetos/status/PersonagemGame.java
@@ -0,0 +1,46 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual > 0) {
+            this.status = "vivo";
+        } else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        int novaSaude = this.saudeAtual - quantidadeDeDano;
+        if (novaSaude < 0) {
+            novaSaude = 0;
+        }
+        setSaudeAtual(novaSaude);
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        int novaSaude = this.saudeAtual + quantidadeDeCura;
+        if (novaSaude > 100) {
+            novaSaude = 100;
+        }
+        setSaudeAtual(novaSaude);
+    }
+}
